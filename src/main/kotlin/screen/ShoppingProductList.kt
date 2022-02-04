deccd906d92c4bbf36a49f35b7a5f0735f8acc35
WonLee <wo0313@nate.com>
2022-02-04 16:58:20 +0900

    단계 4-1. 장바구니 상품 관리를 위한 오브젝트 CartItems, 장바구니 상품 목록 출력을 위한 ShoppingCart 클래스 생성

--- src/main/kotlin/screen/ShoppingProductList.kt
+++ src/main/kotlin/screen/ShoppingProductList.kt
@@ -1,42 +1,100 @@
 package screen
 
+import data.CartItems
 import data.Product
 
-class ShoppingProductList{
+class ShoppingProductList {
+
     private val products = arrayOf(
-        Product("패션","겨울패딩"),
+        Product("패션", "겨울패딩"),
         Product("패션", "겨울바지"),
-        Product("전자기기","핸드폰"),
-        Product("전자기기","블루투스 이어폰"),
-        Product("전자기기","노트북"),
-        Product("반려동물용품","건식 사료"),
-        Product("반려동물용품","습식 사료"),
-        Product("반려동물용품","치약"),
-        Product("반려동물용품","간식")
+        Product("전자기기", "핸드폰"),
+        Product("전자기기", "블루투스 이어폰"),
+        Product("전자기기", "노트북"),
+        Product("반려동물용품", "건식사료"),
+        Product("반려동물용품", "습식사료"),
+        Product("반려동물용품", "치약"),
+        Product("반려동물용품", "간식"),
     )
 
+    /*
+        카테고리별 상품목록 변수 저장
+
+        패션 - ("패션","겨울패딩"),("패션","겨울바지")
+        전자기기 - ("전자기기","핸드폰"),("전자기기","블루투스 이어폰")
+     */
     private val categories: Map<String, List<Product>> =
         products.groupBy { products ->
             products.categoryLabel
         }
+
     /*
         사용자가 입력한 카테고리 정보를 받아 해당 카테고리의 상품을 출력하는 함수
      */
     fun showProducts(selectedCategory: String) {
-        //지정된 카테고리의 상품목록을 가져온다
-        val categoryProducts = categories[selectedCategory]
-        // """"은 해당 쓰여진 내용 그대로 출력되게끔 하는 마크임 \n 사용 안해도됨
-        if(!categoryProducts.isNullOrEmpty()) {
-            println(""" 
-                ***=========================***
+
+        // 지정된 카테고리의 상품목록을 가져온다.
+        val categoryProducts = categories[selectedCategory] // 키: selectedCategory
+
+        // 가져온 상품목록이 비어있지 않으면
+        if (!categoryProducts.isNullOrEmpty()) {
+            println(
+                """
+                ------------------------------
+                
                 선택하신 [$selectedCategory] 카테고리 상품입니다.
-            """.trimIndent())
-            val productSize = categoryProducts.size
-            for (index in 0 until productSize) {
+                
+                ------------------------------
+            """.trimIndent()
+            )
+
+            /*
+            val productsize = categoryProducts.size // 각 카테고리의 size를 담는 변수
+
+            for (index in 0 until productsize) {
                 println("${index}.${categoryProducts[index].name}")
             }
+            */
+
+            categoryProducts.forEachIndexed { index, product ->
+                println("${index}. ${product.name}")
+            }
+
+            // 장바구니에 담을 상품 선택
+            showCartOption(categoryProducts, selectedCategory)
         } else {
             showEmptyProductMessage(selectedCategory)
+        }
+    }
+
+    private fun showCartOption(categoryProducts: List<Product>, selectedCategory: String) {
+
+        println(
+            """
+             ------------------------------
+             
+             장바구니에 담을 상품 번호를 선택해 주세요
+
+        """.trimIndent()
+        )
+
+        // 상품 번호 입력수행
+        val selectedIndex = readLine()?.toIntOrNull()!!
+
+        categoryProducts.getOrNull(selectedIndex)?.let { product ->
+            CartItems.addProduct(product)
+            println("=> 장바구니로 이동하시려면 #을, 계속 쇼핑하려면 * 입력해 주세요")
+
+            val answer = readLine()
+
+            if (answer == "#") {
+                val shoppingCart = ShoppingCart()
+                shoppingCart.showCartItems()
+            } else if (answer == "*") {
+                showProducts(selectedCategory)
+            } else {
+
+            }
         }
     }
 
